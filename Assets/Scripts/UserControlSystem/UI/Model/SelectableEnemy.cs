0bdb5e1e84450c00055ba7ffaf76cc5c1c710acf
Алексей Скворцов <sotoder@gmail.com>
2022-03-31 01:16:38 +0300

    Add NameSpace

--- Assets/Scripts/UserControlSystem/UI/Model/SelectableEnemy.cs
+++ Assets/Scripts/UserControlSystem/UI/Model/SelectableEnemy.cs
@@ -1,7 +1,10 @@
 using Abstractions;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = nameof(SelectableEnemy), menuName = "Strategy Game/" + nameof(SelectableEnemy), order = 0)]
-public class SelectableEnemy : BaseModel<ISelectableEnemy>
+namespace UserControlSystem
 {
+    [CreateAssetMenu(fileName = nameof(SelectableEnemy), menuName = "Strategy Game/" + nameof(SelectableEnemy), order = 0)]
+    public class SelectableEnemy : BaseModel<ISelectableEnemy>
+    {
+    }
 }
